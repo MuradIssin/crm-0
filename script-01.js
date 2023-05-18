d870e59e2a8c76271f3f12f4ebb0f67b7334748e
MuradIssin <117025448+MuradIssin@users.noreply.github.com>
2023-05-18 17:46:20 +0600

    Update script-01.js

--- script-01.js
+++ script-01.js
@@ -1,1 +1,10 @@
-'use sticvt'
+'use strict';
+
+console.log('Модуль 4 урок 1 ');
+let nameProduct = "Картон"
+let quantityProduct = 100;
+let categoryProduct = "упакова"
+let priceProduct = 4500;
+
+console.log('Наименование товара: ' + nameProduct);
+console.log('Общая сумма товаров: ' + quantityProduct * priceProduct);
