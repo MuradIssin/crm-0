68e863777a82880bad1dfe9040fb8e9759e6b948
MuradIssin <117025448+MuradIssin@users.noreply.github.com>
2023-05-19 15:31:28 +0600

    Update script-01.js

--- script-01.js
+++ script-01.js
@@ -1,10 +1,16 @@
 'use strict';
+const rainbow = 'padding: 10px 5px; margin-bottom: 6px; border: 1px solid blue; text-shadow: 1px 1px 2px white,  0 0 0.8; color: orange';
 
-console.log('Модуль 4 урок 1 ');
-let nameProduct = "Картон"
+// модуль 4 урок 1 
+console.log('%c Модуль 4 урок 1 ', rainbow);
+let nameProduct = "Картон";
 let quantityProduct = 100;
-let categoryProduct = "упакова"
+let categoryProduct = "упакова";
 let priceProduct = 4500;
 
 console.log('Наименование товара: ' + nameProduct);
 console.log('Общая сумма товаров: ' + quantityProduct * priceProduct);
+
+
+// модуль 4 урок 2
+console.log('%c Модуль 4 урок 2 ', rainbow);
